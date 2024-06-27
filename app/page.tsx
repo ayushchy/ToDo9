d60817885a34197798518ef05fe844e8358c6301
Adarsh Bhardwaj <madyoffficial@gmail.com>
2024-06-27 22:32:10 +0530

    Change done

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex h-screen w-full justify-center items-center">
-      <ToDo/>
+      <ToDo />
+      <p>Adarsh Bhardwaj</p>
     </div>
   );
 }
